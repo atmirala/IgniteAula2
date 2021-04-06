155413e2832c052110bb73d0bcef29c68d558d86
atmirala <atmirala@gmail.com>
2021-04-06 02:18:18 -0300

    last commit

--- dist/server.js
+++ dist/server.js
@@ -4,5 +4,7 @@ var __importDefault = (this && this.__importDefault) || function (mod) {
 };
 Object.defineProperty(exports, "__esModule", { value: true });
 var express_1 = __importDefault(require("express"));
+var routes_1 = require("./routes");
 var app = express_1.default();
 app.listen(3333);
+app.get("/", routes_1.createCourse);
--- src/routes.ts
+++ src/routes.ts
@@ -7,6 +7,11 @@ export function createCourse(request: Request, response: Response) {
         name: "NodeJS",
         educator: "Dani",
         duration: 10
+    });
+
+    CreateCourseService.execute({
+        name: "ReactJS",
+        educator: "Diego",
     })
 
     return response.send()
